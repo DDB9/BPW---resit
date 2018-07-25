117d888f2229532f0f6e689cb407c6b4b39de0a7
Daan de Bruijn <ddebruijn9@gmail.com>
2018-07-25 13:09:55 +0200

    Ok I think I fixed my git problems
    Now on wards to a good grade!

--- BPW1 - Herkansing/Assets/Scripts/playerController.cs
+++ BPW1 - Herkansing/Assets/Scripts/playerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+	public float lol;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,6 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
 }
