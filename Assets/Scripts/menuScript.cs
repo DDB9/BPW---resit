b1e322df36865041ed4b7f7cce4f903ba249d788
Daan de Bruijn <ddebruijn9@gmail.com>
2018-07-31 18:16:10 +0200

    Finished level 2, added the menu screens and the player can now die
    Long live death.

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+	void Start(){
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	public void playGame(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void mainMenu(){
+		SceneManager.LoadScene(0);
+	}
+
+	public void quit(){
+		Application.Quit();
+	}
+}
