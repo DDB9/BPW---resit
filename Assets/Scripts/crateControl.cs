246c629707443bb4df26370036eb2e01b8e471f5
Daan de Bruijn <ddebruijn9@gmail.com>
2018-08-07 17:43:59 +0200

    Built the game for a playtest, added crates
    Everything's coming along nicely

---
+++ Assets/Scripts/crateControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crateControl : MonoBehaviour {
+
+	public GameObject crate;
+	public GameObject gate;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag != "arm"){
+			crate.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+		} else if (other.tag == "arm"){
+			crate.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+		}
+
+		if (other.tag == "collider"){
+			gate.SetActive(false);
+		}
+	}
+}
